8c670e6d36cd7f77a6196fe99417b6de68bd86f2
Zeeshan Haider <141750645+zeeshiboy07@users.noreply.github.com>
2026-03-30 17:26:33 +0500

    Updates

--- assets/Navbar-WiSzsUxR.js
+++ assets/Navbar-WiSzsUxR.js
@@ -1,4 +1,4 @@
-import{j as P,r as bt}from"./index-CMyL8K8B.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j as P,r as bt}from"./index-BbRlMgb4.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
